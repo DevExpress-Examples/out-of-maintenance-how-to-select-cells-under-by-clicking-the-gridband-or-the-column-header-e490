ad2fe51f3418f2828340556fe6db606a42958d5d
Donate_ro <joke4789@ya.ru>
2019-01-30 14:08:40 +0300

    DataSource is replaced from the database file to the dynamical DataTable object

--- CS/Q130069/Form1.cs
+++ CS/Q130069/Form1.cs
@@ -1,47 +1,65 @@
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Text;
-using System.Windows.Forms;
+using DevExpress.Utils;
 using DevExpress.XtraGrid.Views.BandedGrid;
 using DevExpress.XtraGrid.Views.BandedGrid.ViewInfo;
-using DevExpress.Utils;
+using System;
+using System.Data;
+using System.Windows.Forms;
 
-namespace Q130069 {
-    public partial class Form1 : Form {
-        public Form1() {
+namespace Q130069
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e) {
-            // TODO: This line of code loads data into the 'carsDBDataSet.Cars' table. You can move, or remove it, as needed.
-            this.carsTableAdapter.Fill(this.carsDBDataSet.Cars);
-
-        }
-
-        private void bandedGridView1_MouseDown(object sender, MouseEventArgs e) {
+        private void bandedGridView1_MouseDown(object sender, MouseEventArgs e)
+        {
             if (Control.ModifierKeys != (Keys.Shift | Keys.Control)) return;
             BandedGridView view = (BandedGridView)sender;
             BandedGridHitInfo hInfo = view.CalcHitInfo(e.Location);
-            if (hInfo.InColumn) {
+            if (hInfo.InColumn)
+            {
                 view.ClearSelection();
                 SelectCells(hInfo.Column);
-            } else if (hInfo.InBandPanel && hInfo.Band != null) {
+            }
+            else if (hInfo.InBandPanel && hInfo.Band != null)
+            {
                 view.ClearSelection();
                 SelectCells(hInfo.Band);
-            } else
+            }
+            else
                 return;
             ((DXMouseEventArgs)e).Handled = true;
         }
 
-        private void SelectCells(BandedGridColumn column) {
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bindingSource1.DataSource = GetPersonDataTable();
+
+        }
+        DataTable GetPersonDataTable()
+        {
+            DataTable table = new DataTable();
+            table.TableName = "Persons";
+            table.Columns.Add(new DataColumn("FirstName", typeof(string)));
+            table.Columns.Add(new DataColumn("SecondName", typeof(string)));
+            table.Columns.Add(new DataColumn("Age", typeof(int)));
+            table.Columns.Add(new DataColumn("ID", typeof(int)));
+            for (int i = 0; i < 50; i++)
+                table.Rows.Add("FirstName " + i, "SecondName " + i, 20 + i / 2, i);
+            return table;
+        }
+
+        private void SelectCells(BandedGridColumn column)
+        {
             for (int i = 0; i < column.View.RowCount; i++)
                 column.View.SelectCell(i, column);
         }
 
-        private void SelectCells(GridBand band) {
+        private void SelectCells(GridBand band)
+        {
             foreach (BandedGridColumn column in band.Columns)
                 SelectCells(column);
         }
